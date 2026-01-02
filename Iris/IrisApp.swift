79d259f5e88608dab78beb5d6a34ed8d149c3692
peirunwei-amoeba <peirunwei@gmail.com>
2026-01-02 14:42:12 +0800

    Start project
    starting with the initial files

---
+++ Iris/IrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrisApp.swift
+//  Iris
+//
+//  Created by Runwei Pei on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct IrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
